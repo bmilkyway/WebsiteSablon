33928b2f21be2fea4b909c4a9b1d1f562f3d9463
Bajár Milán <63409632+bmilkyway@users.noreply.github.com>
2025-04-21 21:11:07 +0200

    create project

---
+++ website_template/src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function HomePage(){
+  return(
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li><a href="#selection1">Kezdőlap</a></li>
+            <li><a href="#selection2">Rólunk</a></li>
+            <li><a href="#selection3">Kapcsolat</a></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  )
+}
